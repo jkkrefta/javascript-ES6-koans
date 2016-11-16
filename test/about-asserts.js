7b8289a874c98fc4119d1b13fd945ad5b7ca3387
Jaroslaw Krefta <jaroslaw.krefta@powel.com>
2016-11-16 09:40:29 +0100

    Changed naming after review

--- test/about-asserts.js
+++ test/about-asserts.js
@@ -1,22 +1,22 @@
 import {assert} from "chai";
 import {__} from "../src/helpers";
 
-describe("About Assert", () => {
+describe("About Asserts", () => {
 
   // We shall contemplate truth by testing reality, via asserts
-  it("assert truth", () => {
+  it("Test assert truth", () => {
     assert.isTrue(false); // This should be true
   });
 
   // Enlightenment may be more easily achieved with appropriate
   // messages.
-  it("assert with message", () => {
+  it("Test assert with message", () => {
     assert.isTrue(false, "This should be true -- Please fix this");
   });
 
   // To understand reality, we must compare our expectations against
   // reality.
-  it("assert equality", () => {
+  it("Test assert equality", () => {
     var expected = __;
     var actual_value = 1 + 1;
 
@@ -24,7 +24,7 @@ describe("About Assert", () => {
   });
 
   //Some ways of asserting equality are better than others.
-  it("better way of asserting equality", () => {
+  it("Test a better way of asserting equality", () => {
     var expected = __;
     var actual_value = 1 + 1;
 
@@ -32,7 +32,7 @@ describe("About Assert", () => {
   });
 
   // Sometimes we will ask you to fill in the values
-  it("fill in values", () => {
+  it("Fill in value", () => {
     assert.equal(__, 1 + 1);
   });
 
--- test/about-strings.js
+++ test/about-strings.js
@@ -1,24 +1,26 @@
-import { assert } from "chai";
+/* eslint quotes: ["off", "single"] */
+import { assert, expect } from "chai";
 import { __ } from "../src/helpers";
 
 describe("About Strings", () => {
-  it("Double quoted expressions are strings", () => {
+  it("Test that double quoted expressions are strings", () => {
     var doubleQuotedString = "Hello World";
     assert.isString(__);
   });
 
-  it("Single quoted expressions are strings too!", () => {
+  it("Test single quoted expressions are strings too!", () => {
     var singleQuotedString = 'Hello Again :)';
     assert.isString(__);
   });
 
-  it("Backslash in strings serves for escaping characters", () => {
+  it("Test sackslash in strings serves for escaping characters", () => {
     var stringWithBackslashes = "Hello I\'m String";
     assert.isString(__);
   });
 
-  it("Strings can be created as instance of type String", () => {
+  it("Test strings can be created as instance of type String", () => {
     var stringInstance = new String("You can do this");
     assert.isString(__);
   });
+
 });
